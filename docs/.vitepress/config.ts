daf657909a62e61b97cd9d9b3d4d6988ea18e2d3
bluehyssopu <1767807919@qq.com>
2022-07-30 10:24:13 +0800

    完成初步搭建

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,42 +1,54 @@
-import Unocss from 'unocss/vite'
-import { defineConfig } from 'vitepress'
-import Inspect from 'vite-plugin-inspect'
+import Unocss from "unocss/vite";
+import { defineConfig } from "vitepress";
+import Inspect from "vite-plugin-inspect";
 
 export default defineConfig({
-	lang: 'zh-CN',
-	title: 'Hero',
-	description: '基于 vitepress 的文档模板',
-	head: [['link', { rel: 'icon', href: '/logo.svg' }]],
-	themeConfig: {
-		logo: 'logo.svg',
-		lastUpdated: true,
-		nav: [
-			{
-				text: '指南',
-				link: '/guide/'
-			},
-			{
-				text: '关于',
-				link: '/about/'
-			},
-			{
-				text: 'Gitee',
-				link: 'https://github.com/name/repo'
-			},
-			{
-				text: 'GitHub',
-				link: 'https://github.com/name/repo'
-			}
-		]
-	},
-	vite: {
-		plugins: [
-			Inspect({
-				enabled: false
-			}),
-			Unocss({
-				mode: 'per-module'
-			})
-		]
-	}
-})
+  lang: "zh-CN",
+  title: "客怎眠qvq",
+  description: "基于 vitepress 的小说网站",
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  themeConfig: {
+    logo: "logo.svg",
+    lastUpdated: true,
+    nav: [
+      {
+        text: "小说",
+        link: "/novel/",
+      },
+      {
+        text: "关于",
+        link: "/about/",
+      },
+      {
+        text: "Blog",
+        link: "https://www.kzmqvq.com",
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/bluehyssopu",
+      },
+    ],
+    sidebar: {
+      "/novel/": [
+        {
+          text: "简介",
+          link: "/novel/index",
+        },
+        {
+          text: "第一章",
+          link: "/novel/theme",
+        },
+      ],
+    },
+  },
+  vite: {
+    plugins: [
+      Inspect({
+        enabled: false,
+      }),
+      Unocss({
+        mode: "per-module",
+      }),
+    ],
+  },
+});
--- docs/index.md
+++ docs/index.md
@@ -1,22 +1,8 @@
 ---
 home: true
 heroImage: /logo.svg
-heroText: HeroText
-tagline: This is the Tagline
-actionText: Get Started 👉
-actionLink: /guide/
-features:
-  - title: 🦖 First
-    details: TODO...
-  - title: 🦕 Second
-    details: TODO...
-  - title: 🐸 Third
-    details: TODO...
-  - title: 🐳 Fourth
-    details: TODO...
-  - title: 🐬 Fifth
-    details: TODO...
-  - title: 🐟 Sixth
-    details: TODO...
-footer: --- Thanks for VuePress 🤗 ---
+heroText: 小说
+tagline: Welcome to my novel-blog
+actionText: 👉开始阅读
+actionLink: /novel/
 ---
